b50d248ef069576bac9d976cec392590c3072b99
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-11-22 22:55:18 +0800

    fixed .sql file, finished main

--- homework-1/main.py
+++ homework-1/main.py
@@ -2,10 +2,40 @@
 import psycopg2
 import csv
 
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='5758'
+)
 
-with open('north_data/customers_data.csv', 'r') as file:
-    customer = csv.reader(file)
-    for row in customer:
-        print(row)
+cur = conn.cursor()
 
+with open('north_data/customers_data.csv', encoding='utf-8') as file:
+    csv.field_size_limit(10**8)
+    for line in csv.reader(file):
+        try:
+            cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (line[0], line[1], line[2]))
 
+        finally:
+            conn.commit()
+
+with open('north_data/employees_data.csv', encoding='utf-8') as file:
+    csv.field_size_limit(10**8)
+    for line in csv.reader(file):
+        try:
+            cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
+                        (line[0], line[1], line[2], line[3], line[4], line[5]))
+        finally:
+            conn.commit()
+
+with open('north_data/orders_data.csv', encoding='utf-8') as file:
+    csv.field_size_limit(10**8)
+    for line in csv.reader(file):
+        try:
+            cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+                        (line[0], line[1], line[2], line[3], line[4]))
+        finally:
+            conn.commit()
+
+conn.close()
