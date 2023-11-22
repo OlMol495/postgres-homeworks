3c1cc56661ce2392476ea51c3cefebca3415120a
Olga Lebedeva <olga.s.lebedeva@outlook.com>
2023-11-22 11:55:48 +0800

    csv in main

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,11 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+with open('north_data/customers_data.csv', 'r') as file:
+    customer = csv.reader(file)
+    for row in customer:
+        print(row)
+
+
